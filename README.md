30393072cb96f473b5d8b43179eb4d85a55526e2
Katie Thornhill <katie.thornhill@citrix.com>
2022-04-22 14:23:20 -0400

    add base of api client

---
+++ README.md
@@ -0,0 +1,5 @@
+# Podio-go
+
+Unofficial, semi-internally maintained Go client for Podio.
+
+The only goal for this is to track https://github.com/kayteh/terraform-provider-oidop. This will never have perfect feature parity unless it's picked up by the Podio team for real.
--- podio.go
+++ podio.go
@@ -1,21 +1,146 @@
 // Package podio provides a Podio API client and authentication.
 package podio
 
-type Client struct{}
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"time"
+)
 
+// Client is a Podio API client.
+type Client struct {
+	httpClient *http.Client
+	options    ClientOptions
+	apiURL     *url.URL
+}
+
+// ClientOptions are the options for a Podio API client.
 type ClientOptions struct {
 	ApiKey    string
 	ApiSecret string
+	ApiURL    string
+	UserAgent string
 }
 
-func NewClient() *Client {
-	return &Client{}
+// NewClient creates a new Podio API client with the given options.
+func NewClient(options ClientOptions) *Client {
+	if options.ApiURL == "" {
+		options.ApiURL = "https://api.podio.com"
+	}
+
+	if options.ApiKey == "" || options.ApiSecret == "" {
+		panic("podio-go: ApiKey and ApiSecret are required")
+	}
+
+	apiURL, err := url.Parse(options.ApiURL)
+	if err != nil {
+		panic(fmt.Errorf("podio-go: failed to parse API URL: %w", err))
+	}
+
+	return &Client{
+		httpClient: &http.Client{
+			Timeout: 30 * time.Second,
+			Transport: &authenticatedTransport{
+				options: options,
+				apiURL:  apiURL,
+			},
+		},
+		options: options,
+		apiURL:  apiURL,
+	}
 }
 
-func (c *Client) AuthenticateWithAuthCode(authCode, redirectURI string) ([]byte, error) {
-	return nil, nil
+// AuthenticateWithCreditentials authenticates with the given credentials.
+func (c *Client) AuthenticateWithCredentials(username, password string) error {
+	oauth, err := c.doOAuthGrant(oAuth2Request{
+		Username:     username,
+		Password:     password,
+		ClientID:     c.options.ApiKey,
+		ClientSecret: c.options.ApiSecret,
+		GrantType:    "password",
+	})
+
+	if err != nil {
+		return err
+	}
+
+	c.httpClient.Transport = &authenticatedTransport{
+		apiToken: oauth.AccessToken,
+		options:  c.options,
+		apiURL:   c.apiURL,
+	}
+
+	return nil
 }
 
-func (c *Client) AuthenticateWithCredentials(username, password string) ([]byte, error) {
-	return nil, nil
+type oAuth2Request struct {
+	GrantType    string `json:"grant_type"`
+	Username     string `json:"username,omitempty"`
+	Password     string `json:"password,omitempty"`
+	ClientID     string `json:"client_id"`
+	ClientSecret string `json:"client_secret"`
+}
+
+type oAuth2Token struct {
+	AccessToken  string `json:"access_token"`
+	TokenType    string `json:"token_type"`
+	ExpiresIn    int    `json:"expires_in"`
+	RefreshToken string `json:"refresh_token"`
+	Ref          struct {
+		Type string `json:"type"`
+		ID   string `json:"id"`
+	} `json:"ref"`
+}
+
+func (c *Client) doOAuthGrant(credentials oAuth2Request) (*oAuth2Token, error) {
+	body := &bytes.Buffer{}
+	err := json.NewEncoder(body).Encode(credentials)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.httpClient.Post("/oauth/token", "application/x-www-form-urlencoded", body)
+	if err != nil {
+		return nil, fmt.Errorf("podio-go: failed to request OAuth token: %w", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("podio-go: failed to request OAuth token: %s", resp.Status)
+	}
+
+	var token *oAuth2Token
+	err = json.NewDecoder(resp.Body).Decode(token)
+	if err != nil {
+		return nil, fmt.Errorf("podio-go: failed to decode OAuth token: %w", err)
+	}
+
+	return token, nil
+}
+
+type authenticatedTransport struct {
+	apiToken string
+	apiURL   *url.URL
+	options  ClientOptions
+}
+
+func (a *authenticatedTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	if a.options.UserAgent != "" {
+		req.Header.Add("user-agent", a.options.UserAgent)
+	} else {
+		req.Header.Add("user-agent", "podio-go")
+	}
+
+	if a.apiToken != "" {
+		req.Header.Add("authorization", "OAuth2 "+a.apiToken)
+	}
+
+	if req.URL.Host == "" {
+		req.URL.Scheme = a.apiURL.Scheme
+		req.URL.Host = a.apiURL.Host
+	}
+
+	return http.DefaultTransport.RoundTrip(req)
 }
